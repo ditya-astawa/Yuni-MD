ae9bfaa76b334a2f7d83b40f3e78d08e7baa53d0
ditya-astawa <115558329+ditya-astawa@users.noreply.github.com>
2022-11-25 19:58:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-global.you = ['6283863727744'] 
+global.you = ['6285237596750'] 
 //Developer Bot
-global.gc = 'https://chat.whatsapp.com/IqFfoXcdh7UFyPoQTpKoQp'
-global.rowner = ['6283863727744'] // Real Owner Bot
-global.owner = ['6283863727744'] // Owner Bot
-global.police = ['6283863727744'] // Police Bot
-global.mods = ['6283863727744']  // Want some help?
-global.prems = ['6283863727744'] // Premium user has unlimited limit
+global.gc = 'https://chat.whatsapp.com/Ej8JuTdjGnA4XxLGbwlpMP'
+global.rowner = ['6285237596750'] // Real Owner Bot
+global.owner = ['6285237596750'] // Owner Bot
+global.police = ['6285237596750'] // Police Bot
+global.mods = ['6285237596750']  // Want some help?
+global.prems = ['6285237596750'] // Premium user has unlimited limit
 global.fotonya1 = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg' //ini kalo mau di ganti silahkan ganti aja
 global.fotonya2 = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg' //gw lupa nih foto apa aja:v
 global.fotonya3 = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg' //ini juga ganti
@@ -13,9 +13,9 @@ global.fotonya4 = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg' //ini juga
 global.fotonya5 = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg' //ini juga ganti
 global.fotonya6 = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg' //ini ganti pakai foto sosialmedia mu
 global.fotonya7 = 'https://telegra.ph/file/6433c5e9e7710aeeffeb5.jpg'
-global.instagram = 'https://instagram.com/kokopangeran_'
+global.instagram = 'https://instagram.com/q.c.ditya'
 
-global.helper = ['6283863727744']
+global.helper = ['6285237596750']
 global.APIs = { // API Prefix
   // name: 'https://website'
   amel: 'https://melcanz.com',
@@ -83,9 +83,9 @@ global.fsizedoc = '564588643' // default 10TB
 global.fpagedoc = '444'
 
 // Sticker WM
-global.packname = 'Yuni-MD'
-global.author = '©ᵈʳкσкσ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×'
-global.wm = 'Yuni-MD | ©ᵈʳкσкσ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×'
+global.packname = 'DityaBotz-MD'
+global.author = '©ᵈʳditya ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×'
+global.wm = 'Dityabot-MD | ©ᵈʳditya ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
